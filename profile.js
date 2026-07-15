dfcfeb56f552a480847b6570b58f45cbbac6b97a
Lopez-DePaz <argen.lopezdepaz.0103@gmail.com>
2026-07-15 06:50:53 -0400

    Update student profile with personal details

--- profile.js
+++ profile.js
@@ -11,7 +11,7 @@ If you do not have a link yet, leave that section as an empty list: []
 */
 
 const studentProfile = {
-  name: "Your Name",
+  name: "Argen Lopez-De Paz",
 
   tagline: "DREAM-High Scholar | Computational Biology Portfolio",
 
@@ -25,7 +25,7 @@ const studentProfile = {
 
   If you do not want to use a photo, leave this as an empty string.
   */
-  photo: "",
+  photo: "profile.jpg",
 
   contact: "Contact information available upon request.",
 
@@ -39,11 +39,11 @@ const studentProfile = {
   rpubs: [
     {
       title: "Introduction to R",
-      url: "https://rpubs.com/"
+      url: "https://rpubs.com/DREAM-High/1446545"
     },
     {
       title: "Finding Patterns with Heatmaps",
-      url: "https://rpubs.com/"
+      url: "https://rpubs.com/DREAM-High/1446073"
     }
   ],
 
@@ -65,7 +65,7 @@ const studentProfile = {
   github: [
     {
       title: "My GitHub Profile",
-      url: "https://github.com/"
+      url: "https://github.com/Lopez-DePaz"
     }
   ]
 };
