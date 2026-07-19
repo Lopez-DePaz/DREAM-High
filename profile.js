2904c33259cb0f3bfa6657c6e30ee3df95d7c436
Lopez-DePaz <argen.lopezdepaz.0103@gmail.com>
2026-07-18 21:53:20 -0400

    Update student name in profile.js

--- profile.js
+++ profile.js
@@ -11,7 +11,7 @@ If you do not have a link yet, leave that section as an empty list: []
 */
 
 const studentProfile = {
-  name: "Argen Lopez-De Paz",
+  name: "Ithax Lopez-De Paz",
 
   tagline: "DREAM-High Scholar | Computational Biology Portfolio",
 
